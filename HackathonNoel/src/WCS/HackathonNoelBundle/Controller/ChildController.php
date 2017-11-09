2de969ba438c5067ba51b988c00457ed8dbed1a6
charlotte45 <charlotte.prieur@yahoo.fr>
2017-11-09 22:28:06 +0100

    création page elfes

--- HackathonNoel/src/WCS/HackathonNoelBundle/Controller/ChildController.php
+++ HackathonNoel/src/WCS/HackathonNoelBundle/Controller/ChildController.php
@@ -5,7 +5,8 @@ namespace WCS\HackathonNoelBundle\Controller;
 use WCS\HackathonNoelBundle\Entity\Child;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Child controller.
---
+++ HackathonNoel/src/WCS/HackathonNoelBundle/Controller/elvesController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder4
+ * Date: 09/11/17
+ * Time: 17:04
+ */
+
+namespace WCS\HackathonNoelBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use WCS\HackathonNoelBundle\Entity\Child;
+use WCS\HackathonNoelBundle\Entity\Gift;
+
+/**
+ * Class elvesController
+ * @package WCS\HackathonNoelBundle\Controller
+ * @Route("elves")
+ */
+class elvesController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/")
+     */
+    public function showAll()
+    {
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository(Child::class);
+
+        $listChildren = $repository->findAll();
+
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository(Gift::class);
+
+        $listGifts = $repository->findBy(['done' => 'false']);
+
+        return $this->render('elves/elves.html.twig',
+            array(
+                'children' => $listChildren,
+                'gifts' => $listGifts)
+        );
+    }
+
+
+
+}
