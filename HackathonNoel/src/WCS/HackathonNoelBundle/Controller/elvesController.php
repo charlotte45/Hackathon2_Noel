ad97616b543d567e15e90a41ecbdf9a3ce7de5d1
charlotte45 <charlotte.prieur@yahoo.fr>
2017-11-10 09:16:08 +0100

    route done

--- HackathonNoel/src/WCS/HackathonNoelBundle/Controller/elvesController.php
+++ HackathonNoel/src/WCS/HackathonNoelBundle/Controller/elvesController.php
@@ -61,7 +61,7 @@ class elvesController extends Controller
     /**
      * @param Gift $gift
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     * @Route("/done" name="done")
+     * @Route("/done", name="done")
      */
     public function doneAction(Gift $gift)
     {
