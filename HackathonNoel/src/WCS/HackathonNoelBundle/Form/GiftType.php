5e1067a734017cbf820bd28ea44b203396672970
charlotte45 <charlotte.prieur@yahoo.fr>
2017-11-10 13:20:22 +0100

    modif affichage page jouets

--- HackathonNoel/src/WCS/HackathonNoelBundle/Form/GiftType.php
+++ HackathonNoel/src/WCS/HackathonNoelBundle/Form/GiftType.php
@@ -3,6 +3,7 @@
 namespace WCS\HackathonNoelBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,9 @@ class GiftType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name' );
+        $builder->add('name',       TextType::class, array(
+            'label' => "Quel(s) cadeau(x) souhaites-tu ?",
+        ));
     }
     
     /**
